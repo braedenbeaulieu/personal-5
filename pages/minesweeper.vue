da8d1d7242f5aba88e68b8865d50de4fc9983d11
Braeden Beaulieu <braedenbeaulieu@gmail.com>
2022-12-29 16:51:08 -0500

    added minesweeper page

---
+++ pages/minesweeper.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <section class="bg-gray-200 p-2 m-4 border-2 border-black col-span-12">
+            <h1>Minesweeper</h1>
+            <Minesweeper />
+        </section>
+    </div>
+</template>
+<script setup lang="ts">
+    
+</script>
+<style lang="scss">
+</style>
